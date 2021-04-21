575128a6c199db490334d12a39656e85a04db64f
sudharsan2000 <ssudharsan2000@gmail.com>
2021-04-21 16:44:02 +0530

    LED controls added

--- NodeMCU/Hardware_Handle/Hardware_Handle.ino
+++ NodeMCU/Hardware_Handle/Hardware_Handle.ino
@@ -14,9 +14,61 @@
 #define PASS "q7qfia5b"
 #define WATER_PUMP 5
 #define LED 4
+#define BUZZER 14
+
+#define BUZZER_DELAY 500  // Time in milli seconds for the buzzer
+#define LED_DELAY 100
 
 ESP8266WebServer server(80);
 
+bool buzzer_status;
+unsigned long t2;
+unsigned long t1;
+unsigned long t3;
+unsigned long t4;
+bool x = 1;
+
+int LED_ARRAY[5] = {16, 0, 12, 13, 4};
+uint8_t LED_ARRAY_STATUS = 0;
+void write_LED_array(uint8_t LED_status)
+{
+  digitalWrite(LED_ARRAY[0], (LED_status & 0b00001));
+  digitalWrite(LED_ARRAY[1], (LED_status & 0b00010));
+  digitalWrite(LED_ARRAY[2], (LED_status & 0b00100));
+  digitalWrite(LED_ARRAY[3], (LED_status & 0b01000));
+  digitalWrite(LED_ARRAY[4], (LED_status & 0b10000));
+}
+void handle_LED_array()
+{
+   if(buzzer_status)
+  {
+    t4=millis();
+    if (t4-t3 >= LED_DELAY)
+    {
+      t3 = millis();
+      write_LED_array(LED_ARRAY_STATUS);
+      LED_ARRAY_STATUS = (LED_ARRAY_STATUS == 0) ? 0b00001 : LED_ARRAY_STATUS<<1;
+    }
+  }
+  else
+    write_LED_array(0);
+}
+void handle_buzzer()
+{
+  if(buzzer_status)
+  {
+    t2=millis();
+    if (t2-t1 >= BUZZER_DELAY)
+    {
+      t1 = millis();
+      digitalWrite(BUZZER,x);
+      
+      x = !x;
+    }
+  }
+  else
+    digitalWrite(BUZZER,0);
+}
 void handleRoot() 
 {
   server.send(200, "text/plain", "Hello\n 1. /pumpon to turn on the pump\n 2. /pumpoff to turn off the pump \n 3. /ledon to turn on LEDs\n 4. /ledoff to turn off LEDs ");
@@ -34,18 +86,27 @@ void pump_off()
 void LED_on() 
 {
   digitalWrite(LED, HIGH);
+  buzzer_status = 1;
   server.send(200, "text/plain", "LED ON!!!");
 }
 void LED_off() 
 {
   digitalWrite(LED, LOW);
+  buzzer_status = 0;
   server.send(200, "text/plain", "LED OFF!!!");
 }
 void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(LED, OUTPUT);
-    pinMode(WATER_PUMP, OUTPUT);\
+    pinMode(WATER_PUMP, OUTPUT);
+    pinMode(BUZZER, OUTPUT);
+
+    pinMode(LED_ARRAY[0], OUTPUT);
+    pinMode(LED_ARRAY[1], OUTPUT);
+    pinMode(LED_ARRAY[2], OUTPUT);
+    pinMode(LED_ARRAY[3], OUTPUT);
+    pinMode(LED_ARRAY[4], OUTPUT);
     
     digitalWrite(LED, LOW);
     digitalWrite(WATER_PUMP, LOW);
@@ -83,5 +144,6 @@ void setup()
 void loop()
 {
   server.handleClient();
-
+  handle_LED_array();
+  handle_buzzer();
 }
